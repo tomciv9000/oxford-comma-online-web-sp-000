32bc35575f7ad2f17407f4d67f470980e44e71c9
Tom White <eugene.white@gmail.com>
2019-05-16 18:06:58 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array[0]
+  elsif array.length == 2 
+    array.join(" and ")
+  else
+    string = ""
+    i = 0 
+    while i < (array.length - 1)
+    string << "#{array[i]}, "
+    i += 1 
+  end
+  string + "and #{array[-1]}"
+end
 end
